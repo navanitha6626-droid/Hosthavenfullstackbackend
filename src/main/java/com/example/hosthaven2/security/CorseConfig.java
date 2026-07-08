d21aef3fae55a1972252ec56acd66a38231f013b
navanitha6626-droid <navanitha6626@gmail.com>
2026-07-08 14:50:25 +0530

    Update CorseConfig.java

--- src/main/java/com/example/hosthaven2/security/CorseConfig.java
+++ src/main/java/com/example/hosthaven2/security/CorseConfig.java
@@ -20,7 +20,7 @@ public class CorseConfig {
                 "http://localhost:5500",
                 "http://127.0.0.1:5500",
                 "http://localhost:5000",
-                "http://127.0.0.1:5000"));
+                "http://127.0.0.1:5000","https://navanitha6626-droid.github.io/Hosthavenfullstackfrontend/" ));
 
         config.setAllowedMethods(List.of(
                 "GET",
